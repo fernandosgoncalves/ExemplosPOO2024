130d38410f8c166179931f6a976a2ced2212b4a6
aluno <aluno>
2024-10-11 21:54:14 -0300

    Final 11/10

---
+++ MetodoEstatico/src/entidades/Quadrado.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entidades;
+
+/**
+ *
+ * @author aluno
+ */
+public class Quadrado {
+    public double lado;
+    public double perimetro;
+    public double area;
+
+    @Override
+    public String toString() {
+        return "Quadrado{" + "lado=" + lado + ", perimetro=" + perimetro + ", area=" + area + '}';
+    }
+    
+    
+    
+}
